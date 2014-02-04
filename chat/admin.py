e02feaaee3747065062e8e22c6ae2ec86f58df25
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2014-02-04 22:03:56 +0000

    start building models

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from chat.models import Room, Line
 
 # Register your models here.
+class LinesInline(admin.TabularInline):
+    model = Line
+
+class RoomAdmin(admin.ModelAdmin):
+    inlines = [LinesInline]
+
+admin.site.register(Room, RoomAdmin)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    def __unicode__(self):
+        return "Room %d" % self.id
+
+class Line(models.Model):
+    room = models.ForeignKey(Room)
+    text = models.CharField(max_length=500)
+
+    def __unicode__(self):
+        return self.text
