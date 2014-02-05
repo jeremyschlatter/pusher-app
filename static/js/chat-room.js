9ba79667459b2789f016dfdf692b7e5d1d7d8eba
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2014-02-05 04:32:41 +0000

    each chat room has its own channel, can receive updates from it

--- static/js/chat-room.js
+++ static/js/chat-room.js
@@ -8,7 +8,7 @@ Pusher.log = function(message) {
 var pusher = new Pusher('0e0c5944e77aaa1238f8');
 var channel;
 function connect(name) {
-    channel = pusher.subscribe('test_channel');
+    channel = pusher.subscribe(name);
     channel.bind('new_message', function(data) {
         $('#chat-lines').append('<li>' + data.message + '</li>');
     });
