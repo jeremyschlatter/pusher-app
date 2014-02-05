2196abc2ad2d66903cc7619e04d04ed4fc85c7d7
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2014-02-05 20:26:41 +0000

    realtime notifications working

--- chat/views.py
+++ chat/views.py
@@ -4,6 +4,13 @@ from django.core.urlresolvers import reverse
 
 from chat.models import Room
 
+import pusher
+p = pusher.Pusher(
+  app_id='65252',
+  key='0e0c5944e77aaa1238f8',
+  secret='6fbab91e75fd738c2386'
+)
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, Jeremy")
@@ -20,4 +27,5 @@ def submit(request, room_id):
         return render(request, 'rooms/get.html', {'room': room, 'error': 'No text submitted'})
     room.line_set.create(text=text)
     room.save()
-    return HttpResponseRedirect(reverse('chat:room', args=(room.id,)))
+    p['room' + room_id].trigger('new_message', {'message': text})
+    return HttpResponseRedirect(reverse('chat:room', args=(room_id,)))
--- static/js/chat-room.js
+++ static/js/chat-room.js
@@ -1,3 +1,17 @@
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+var csrftoken = $.cookie('csrftoken');
+$.ajaxSetup({
+    crossDomain: false, // obviates need for sameOrigin test
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type)) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
 // Enable pusher logging - don't include this in production
 Pusher.log = function(message) {
   if (window.console && window.console.log) {
@@ -5,11 +19,16 @@ Pusher.log = function(message) {
   }
 };
 
-var pusher = new Pusher('0e0c5944e77aaa1238f8');
+var pusher = new Pusher('0e0c5944e77aaa1238f8', {'encrypted': true});
 var channel;
 function connect(name) {
     channel = pusher.subscribe(name);
     channel.bind('new_message', function(data) {
         $('#chat-lines').append('<li>' + data.message + '</li>');
     });
-};
+}
+
+function doSubmit(submitUrl) {
+    $.post(submitUrl, {'text': $('#new-line').val()});
+    $('#new-line').val('');
+}
